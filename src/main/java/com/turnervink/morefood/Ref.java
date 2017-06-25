855b7b5c9b81c02225ed2be76d796f57a10ae151
Turner Vink <turner@turnervink.com>
2017-06-24 17:57:19 -0700

    Bump version to 1.1_a

--- src/main/java/com/turnervink/morefood/Ref.java
+++ src/main/java/com/turnervink/morefood/Ref.java
@@ -7,7 +7,7 @@ public class Ref {
 
     public static final String MODID = "morefood";
     public static final String NAME = "More Food Mod";
-    public static final String VERSION = "1.0_a";
+    public static final String VERSION = "1.1_a";
 
     public static final String CLIENT_PROXY = "com.turnervink.morefood.proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.turnervink.morefood.proxy.CommonProxy";
